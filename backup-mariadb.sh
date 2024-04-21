97c3e9076445f04445fcd80a25dba1df17dbefad
stephaneworkspace <stephane@bressani.dev>
2024-04-21 15:57:29 +0000

    ok

--- backup-mariadb.sh
+++ backup-mariadb.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-/usr/bin/mariadb-dump --defaults-file=/home/arch/backup_mariadb/my.cnf bress_db > bress_db_$(date +%Y-%m-%d).sql
 
+source /home/arch/swift-env/bin/activate
+cd /home/arch/backup_mariadb/
+CONTAINER_NAME="default"
+DIRECTORY_NAME="mariadb_arch"
+
+mkdir -p $DIRECTORY_NAME
+#swift post $CONTAINER_NAME/$DIRECTORY_NAME
+
+DATABASES=("bress_db")
+
+DATE=$(date +%F)
+
+for DB_NAME in "${DATABASES[@]}"
+do
+    BACKUP_FILE="${DIRECTORY_NAME}/${DB_NAME}_backup_${DATE}.sql"
+    pg_dump -U postgres -d $DB_NAME > $BACKUP_FILE
+    /usr/bin/mariadb-dump --defaults-file=/home/arch/backup_mariadb/my.cnf $DB_NAME > $BACKUP_FILE
+    swift upload $CONTAINER_NAME $BACKUP_FILE
+    rm $BACKUP_FILE
+done
+
+#swift list $CONTAINER_NAME --prefix $DIRECTORY_NAME/
+rm -rf $DIRECTORY_NAME
